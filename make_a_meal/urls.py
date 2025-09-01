3ad30f500d47e381a9b007a3b4c41c3b6848d17e
korey <olorunfemioluwakorede9@gmail.com>
2025-09-01 23:12:04 +0100

    finished detail, start Signup feature

--- make_a_meal/urls.py
+++ make_a_meal/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from menu.views import index, contact
 
 from django.conf import settings
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('items/', include('item.urls')),
     path('', index, name='index'),
     path('contact/', contact, name='contact')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
