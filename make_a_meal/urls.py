b838a8d3756d10cb3afa19a1f770ea8eab4e1d3e
korey <olorunfemioluwakorede9@gmail.com>
2025-09-02 15:41:08 +0100

    implement signup feature

--- make_a_meal/urls.py
+++ make_a_meal/urls.py
@@ -24,6 +24,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('items/', include('item.urls')),
-    path('', index, name='index'),
-    path('contact/', contact, name='contact')
+    path('', include('menu.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from item.models import Category, Item
+from .forms import SignUpForm
 
 def index(request):
     items = Item.objects.filter(is_sold= False)[0:6]
@@ -11,3 +12,20 @@ def index(request):
 
 def contact(request):
     return render(request, 'menu/contact.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+        
+    else:
+        form = SignUpForm()
+
+    return render (request, 'menu/signup.html',{
+        'form': form,
+    })
